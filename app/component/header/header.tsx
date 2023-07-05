88df378794154ebae1bed7f23096799145a0406a
Phung The Anh <phungtheanh2001@gmail.com>
2023-07-05 11:20:40 +0700

    up

--- app/component/header/header.tsx
+++ app/component/header/header.tsx
@@ -3,12 +3,16 @@
 import Image from "next/image";
 import {BsSearch} from "react-icons/bs";
 import {FaRegUser} from "react-icons/fa";
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 import {GrFormClose} from "react-icons/gr";
 import Link from "next/link";
 import LinkHeader from "@/app/component/header/linkHeader";
 import LinkHeaderMobile from "@/app/component/header/linkHeaderMobile";
 import LinkUser from "@/app/component/header/linkUser";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 
 const Header = () => {
@@ -54,23 +58,37 @@ const Header = () => {
     }
     window.addEventListener("scroll", onScroll)
   })
+  const [age, setAge] = useState('');
 
+  const handleChange = (event: any) => {
+    setAge(event.target.value);
+  };
   return (
     <div ref={headerRef} className='w-full fixed top-0 left-0 z-[99] bg-transparent'>
       {/*search*/}
       <div ref={searchMenuRef}
-           className="searchMenu flex items-center justify-center opacity-0 invisible z-[1] fixed top-[30%] lg:top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] w-[98%] md:w-[80%] lg:w-[84%] xl:w-[70%] transition-all duration-300">
+           className="searchMenu flex items-center justify-center opacity-0 invisible z-[10] fixed top-[30%] lg:top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] w-[98%] md:w-[80%] lg:w-[84%] xl:w-[70%] transition-all duration-300">
         <div className="fixed w-full h-full top-0 left-0 bg-transparent"></div>
         <div
           className="flex lg:flex-row flex-col items-center justify-between gap-[20px] lg:gap-[30px] w-[80%] lg:w-full shadow-[0_0_20px_#00000099] bg-[#4366c5ab] p-[30px] lg:p-[20px] rounded-xl">
           <div className="flex flex-col justify-center items-start gap-[10px] w-full">
-            <p className="text-base xl:text-lg text-white">Địa điểm</p>
-            <select className="py-[10px] px-[30px] w-full outline-0 border-0 rounded-[36px] text-base text-[#adadad]"
-                    name="address">
-              <option value="">Địa điểm</option>
-              <option value="">Nam Định</option>
-              <option value="">Hà Nội</option>
-            </select>
+            {/*<p className="text-base xl:text-lg text-white">Địa điểm</p>*/}
+            <FormControl className="w-full rounded-[36px] bg-white border-0 outline-0" size="small">
+              <InputLabel>Địa điểm</InputLabel>
+              <Select
+                value={age}
+                label="Age"
+                onChange={handleChange}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={18}>Nam Định</MenuItem>
+                <MenuItem value={29}>Hà Nội</MenuItem>
+                <MenuItem value={30}>Thanh Hóa</MenuItem>
+              </Select>
+            </FormControl>
+
           </div>
           <div className="flex flex-col justify-center items-start gap-[10px] w-full">
             <p className="text-base xl:text-lg text-white">Ngày khởi hành</p>
