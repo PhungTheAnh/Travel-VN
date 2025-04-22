aa4607a76a0d65d69aa2de4de1e9a14c57a5480f
anhpt <phungtheanh2001@gmail.com>
2025-04-22 16:50:15 +0700

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,34 @@
-import "./globals.css";
-import {Inter} from "next/font/google";
-import Header from "@/app/component/header/header";
-import Footer from "@/app/component/footer/footer";
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Header from '@/app/component/header/header';
+import Footer from '@/app/component/footer/footer';
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Travel VN",
-  description: "Travel VN by TheAnh",
+  title: 'Travel VN',
+  description: 'Travel VN by TheAnh',
+  name: 'google-site-verification',
+  content: 'eYkQszMmKVo_XuRe-yltvEpS3VPXb2OdOhbiVADJRJM',
 };
 
-export default function RootLayout({children}: { children: React.ReactNode; }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en">
-    <body className={inter.className + ' mx-auto min-h-screen flex flex-col justify-between'}>
-    <Header/>
-    {children}
-    <Footer/>
-    </body>
+    <html lang='en'>
+      <body
+        className={
+          inter.className +
+          ' mx-auto min-h-screen flex flex-col justify-between'
+        }
+      >
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
