5ad8ba2653c0e13bc70d2e32685eb919d2619068
anhpt <phungtheanh2001@gmail.com>
2025-04-23 08:50:01 +0700

    up

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://travel-vn.vercel.app',
-  generateRobotsTxt: true, // nếu bạn muốn tạo luôn robots.txt
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000, // Nếu ít page vẫn cần để tách file con
+  exclude: [], // đừng loại trừ route nào trừ khi thật sự cần
 };
