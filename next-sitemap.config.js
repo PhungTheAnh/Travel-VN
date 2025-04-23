36c417a59bbf0c8d84de20cb92d299704fa6249b
anhpt <phungtheanh2001@gmail.com>
2025-04-23 09:58:34 +0700

    up

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,17 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
-// module.exports = {
-//   siteUrl: 'https://travel-vn.vercel.app',
-//   generateRobotsTxt: true,
-//   changefreq: 'daily',
-//   priority: 0.7,
-//   sitemapSize: 5000, // Nếu ít page vẫn cần để tách file con
-//   exclude: [], // đừng loại trừ route nào trừ khi thật sự cần
-//   outDir: './public', // hoặc './out' nếu dùng export
-// };
-
 module.exports = {
-  siteUrl: 'https://example.com', // phải có dù không dùng
-  generateRobotsTxt: false, // ❌ không generate robots.txt
-  outDir: './', // không cần xuất vào public
-  exclude: ['/**'], // loại trừ tất cả route khỏi sitemap (không tạo file luôn)
+  siteUrl: 'https://travel-vn.vercel.app',
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000, // Nếu ít page vẫn cần để tách file con
+  exclude: [], // đừng loại trừ route nào trừ khi thật sự cần
+  outDir: './public', // hoặc './out' nếu dùng export
 };
